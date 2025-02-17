f97eaa5c9b489d02c1591b311050988f2ffa62f0
Jason <jason95@vt.edu>
2025-02-17 17:04:23 -0500

    ptr

---
+++ Section_15/ptr.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+int MAX = 10;
+//Jason
+int main() {
+
+	//Excercise 1
+	int array[MAX];
+	int *array_ptr = array;
+	for (int index = 0; index < MAX; ++index) {
+		*(array_ptr + index) = 0;
+	}
+
+
+	for (int i = 0; i < MAX; ++i) {
+		std::cout << "Array val: " << array[i] << "\n";
+	}
+
+	char line[] = "Jason Barrett";
+	char delim = ' ';
+	char *first_ptr;
+	char *last_ptr;
+	first_ptr = line;
+	last_ptr = strchr(line, delim);
+	//*(first_ptr + 5) = '\0';
+	*last_ptr = '\0';
+	++last_ptr;
+
+	std::cout << "First: " << first_ptr << " Last:" << last_ptr << "\n";
+
+
+
+	return (0);
+}
